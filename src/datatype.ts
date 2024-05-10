ab6116269a3f79fe9ffed670e92564267ded24ef
zxfishhack <zxfishhack@126.com>
2024-05-10 14:52:39 +0800

    add internal field

--- src/datatype.ts
+++ src/datatype.ts
@@ -261,24 +261,24 @@ export class bytes implements DataType<Uint8Array> {
   }
 }
 
-export function bitCount (n: number) {
-  n = n - ((n >> 1) & 0x55555555)
-  n = (n & 0x33333333) + ((n >> 2) & 0x33333333)
-  return ((n + (n >> 4) & 0xF0F0F0F) * 0x1010101) >> 24
-}
+export class internal<T> implements DataType<T> {
+  private v: T
 
-export function bitSet(n: number, idx: number) {
-  return (n & (1 << idx)) > 0
-}
+  constructor(v: T) {
+    this.v = v
+  }
 
-export function bitReset(n: number, idx: number) {
-  return ((n) ^ (~(1 << idx))) > 0
-}
+  get size() {
+    return 0
+  }
 
-export function makeUInt8Array(...value: number[]): Uint8Array {
-  const res = new Uint8Array(value.length)
-  value.forEach((v, i) => {
-    res[i] = v
-  })
-  return res
+  get value() {
+    return this.v
+  }
+  set value(v: T) {
+    this.v = v
+  }
+
+  public encode(data: DataView, offset: number): void {}
+  public decode(data: DataView, offset: number): void {}
 }
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
 export * from "./datatype"
+export * from "./utils"
